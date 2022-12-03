6c1b1b9047fe57d79daa5fecb5d32c86f9820f07
harshvns <114221468+harshvns@users.noreply.github.com>
2022-12-03 23:40:16 +0530

    code

--- quadraticequation.py
+++ quadraticequation.py
@@ -1,3 +1,7 @@
-a=int(input("a:"))
-b=int(input("b:"))
-c=int(input("c:"))
+a=float(input("a:"))
+b=float(input("b:"))
+c=float(input("c:"))
+d=(b*b-4*a*c)**.5
+r1=(-b+d)/(2*a)
+r2=(-b-d)/(2*a)
+print("r1=",r1,",r2=",r2)
--- sagarbhaiya.py
+++ sagarbhaiya.py
@@ -1,1 +1,13 @@
-print("hello")
+s1 = "dog"
+s2 = "god"
+count = 0
+for i in range(len(s1)):
+    if s1[i] in s2:
+        count += 1
+if len(s1)!=len(s2):
+    print("not anagram")
+
+elif count == len(s1):
+    print("anagram")
+else:
+    print("not anagram")
